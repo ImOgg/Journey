86abdd09c50128f15e8e331fa5e73382f17aa18f
DESKTOP-6ET3TVT\user <a0989077338@gmail.com>
2023-04-07 19:11:28 +0800

    一個關於日本的網頁

---
+++ app.js
@@ -0,0 +1,36 @@
+gsap.to(".typing1", {
+    text: "真的好想去日本玩= =", //text屬性將自動為DOM元素嵌入我們所輸入的文字
+    duration: 1.5,
+    scrollTrigger: {
+              trigger: ".typing1",
+      toggleActions: "play pause resume reset", //見備註
+    },
+  });
+
+  gsap.fromTo(
+    ".cursor",
+    0,
+    {
+      visibility: "hidden",
+    },
+    {
+      visibility: "visible",
+      repeat: -1,
+      yoyo: true, // 若為true，則動畫repeat運行順序將會以倒放的形式回去，如溜溜球一樣
+      repeatDelay: 0.3, // 下一次repeat的delay時間
+    }
+  );
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    keyboard: {
+      enabled: true,
+    },
+  
+    navigation: {
+      nextEl: ".button-next",
+      prevEl: ".button-prev",
+    },
+  });
